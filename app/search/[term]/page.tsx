8a433943c95f15fc63ca81814acfbad4bd661bea
Tim Linsenmayer <tim.linsenmayer@web.de>
2023-10-04 10:12:30 +0200

    💄 Update search result text

--- app/search/[term]/page.tsx
+++ app/search/[term]/page.tsx
@@ -30,12 +30,23 @@ export default function Page({ params }: { params: { term: string } }){
                 </div>
         </div>
     )
+    
+    function getAmountString(val:number){
+        switch(val){
+            case 0: 
+                return "Keine Ergebnisse gefunden"; 
+            case 1: 
+                return "Ein Ergebnis gefunden"; 
+            default: 
+                return val+" Ergebnisse gefunden";
+        }
+    }
     return (
         <div className="flex-1">
             <div className="container">
                 <div className='rounded-lg border bg-card text-card-foreground shadow-sm mt-3 px-5 flex flex-col items-start gap-2 px-4 pt-8 md:pt-12 relative pb-4 md:pb-8 lg:pb-12' id='greeter'>
                     <h1 className='text-2xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]'>Suchergebnis für: {params.term}</h1>
-                    <p className='max-w-[750px] text-lg text-muted-foreground sm:text-xl'>{data.total} Ergebnisse gefunden.</p>
+                    <p className='max-w-[750px] text-lg text-muted-foreground sm:text-xl'>{getAmountString(data.total)}</p>
                 </div>
                 
                 <div className="pt-4">
